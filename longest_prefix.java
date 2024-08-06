2ed5bdd22f764c1b2438d258eed80136c0d2b223
Tanuj <166424073+Incarnate001@users.noreply.github.com>
2024-08-06 19:55:20 +0530

    Create longest_prefix.java

---
+++ longest_prefix.java
@@ -0,0 +1,68 @@
+import java.util.Scanner;
+
+class longest_prefix
+
+{
+
+    public static void main(int terms)
+
+    {
+
+        String a[] = new String[terms];
+
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Enter " + terms + " terms");
+
+        String d = "";
+
+        for(int i = 0;i < terms;i++)
+
+        {
+
+            a[i] = in.nextLine();
+
+            a[i]=a[i].toUpperCase();
+
+            d+= a[i]+" ";
+
+        }
+
+        String e = "";
+
+        String k1 = methods.smallest(d);
+
+        int k = k1.length();
+
+        for(int i = 0;i < k;i++)
+
+        {
+
+            char ch = a[0].charAt(i);
+
+            for(int j = 1;j < terms;j++)
+
+            {
+
+                if(ch != a[j].charAt(i))
+
+                {
+
+                    System.out.println("LoNgEsT PrEfIx : " + e + " \uD83D\uDE0A");
+
+                    System.exit(0);
+
+                }
+
+            }
+
+            e+= ch;
+
+        }
+
+        System.out.println("LoNgEsT PrEfIx : " + e + " \uD83D\uDE0A");
+
+    }
+
+}
+
